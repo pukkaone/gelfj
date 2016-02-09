b3ef1a8e284ae84498f74e9ae3c4ca03bc6bac0c
IgorPerikov <perikov.igor@gmail.com>
2016-02-09 12:49:07 +0300

    fix confirmation settings on amqp channel

--- src/main/java/com/github/pukkaone/gelf/protocol/GelfAMQPSender.java
+++ src/main/java/com/github/pukkaone/gelf/protocol/GelfAMQPSender.java
@@ -73,6 +73,7 @@ public class GelfAMQPSender extends GelfSender {
                     routingKey,
                     properties,
                     toAMQPBuffer(message.toJson()).array());
+                channel.confirmSelect();
                 channel.waitForConfirms();
 
                 return true;
